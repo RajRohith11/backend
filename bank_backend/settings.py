a0c5deb421158b1eba90071635063f7acd28c107
Rajrohith <rajrohith902@gmail.com>
2025-05-20 19:14:01 +0530

    mark 72

--- bank_backend/settings.py
+++ bank_backend/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'bank_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgersql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
